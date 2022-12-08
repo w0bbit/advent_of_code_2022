3be4df0b374eae9ebb531abdd241e9e20acf000a
Robert Ryan <ryanre@gmail.com>
2022-12-08 15:34:25 -0800

    FFFFFFFF!!!!!!!!!

--- day_8/day_8a.pl
+++ day_8/day_8a.pl
@@ -1,108 +1,132 @@
 use v5.36;
 use warnings;
-use Data::Dumper;
+use feature qw(signatures);
 
-sub {
-
-  # converting input into grid of hashes with tree height and visibility bool
-  my @grid;
-  open (my $INPUT, '<', (@ARGV));
-  while (my $line = <$INPUT>) {
-    chomp $line;
-    my @row;
-    for my $val (split '', $line) {
-      my %tree_info = (
-        'ht' => $val,
-        'vis' => 0,
-      );
-      push @row, \%tree_info;
-    }
-    push @grid, \@row;
+my @grid;
+open(my $INPUT, '<', (@ARGV));
+while (my $line = <$INPUT>) {
+  chomp $line;
+  my @line;
+  for (split '', $line) {
+    my %hash = (
+      val => $_,
+      vis => 0,
+    );
+    push @line, \%hash;
   }
-  close $INPUT;
+  push @grid, \@line;
+}
 
-  for my $i (1..@grid-2) {
+my $count = 0;
+my $max = -1;
 
-    # checking visibility from the left
-    my @row = @{$grid[$i]};
-
-    my $max_so_far = ${$row[0]}{'ht'};
-    for my $j (1..@row-2) {
-      my %prev = %{$row[$j-1]};
-      my %curr = %{$row[$j]};
-      if ($curr{'ht'} > $max_so_far) {
-        $max_so_far = $curr{'ht'};
-        ${${$grid[$i]}[$j]}{'vis'} = 1;
-      }
-    }
-
-    # checking visibility from the right
-    @row = reverse @row;
-    $max_so_far = ${$row[0]}{'ht'};
-    for my $j (1..@row-2) {
-      my %prev = %{$row[$j-1]};
-      my %curr = %{$row[$j]};
-      if ($prev{'ht'} < $curr{'ht'}) {
-        $max_so_far = $curr{'ht'};
-        ${${$grid[$i]}[$j]}{'vis'} = 1;
-      }
-    }
-
-  }
-
-  # transpose grid
-  my @tr_grid;
-
-  # https://perlmaven.com/transpose-a-matrix
-  for my $row (0..@grid-1) {
-    for my $col (0..@{$grid[$row]}-1) {
-      $tr_grid[$col][$row] = $grid[$row][$col];
+for my $i (0..@grid-1) {
+  my @row = @{$grid[$i]};
+  my $line;
+  for my $j (0..@row-1) {
+    my %tree = %{$row[$j]};
+    # $line .= $tree{'val'};
+    if ($tree{'val'} > $max) {
+      $max = $tree{'val'};
+      $tree{'vis'} = 1;
+      ${$grid[$i]}[$j]{'vis'} = 1;
+      # $line .= 'V';
+      $line .= $tree{'val'};
+    } else {
+      $line .= $tree{'val'};
     }
   }
+  say $line;
+  $max = -1;
+}
 
-  @grid = @tr_grid;
+say '-----';
 
-  for my $i (1..@grid-2) {
-
-    # checking visibility from the left
-    my @row = @{$grid[$i]};
-    my $max_so_far = ${$row[0]}{'ht'};
-    for my $j (1..@row-2) {
-      my %prev = %{$row[$j-1]};
-      my %curr = %{$row[$j]};
-      if ($curr{'ht'} > $max_so_far) {
-        $max_so_far = $curr{'ht'};
-        ${${$grid[$i]}[$j]}{'vis'} = 1;
-      }
-    }
-
-    # checking visibility from the right
-    @row = reverse @row;
-    $max_so_far = ${$row[0]}{'ht'};
-    for my $j (1..@row-2) {
-      my %prev = %{$row[$j-1]};
-      my %curr = %{$row[$j]};
-      if ($prev{'ht'} < $curr{'ht'}) {
-        $max_so_far = $curr{'ht'};
-        ${${$grid[$i]}[$j]}{'vis'} = 1;
-      }
-    }
-
-  }
-
-  my $count = 0;
-
-  for my $row (@grid) {
-    my @row = @$row;
-    for my $tree (@row) {
-      my %tree = %$tree;
-      if ($tree{'vis'}) {
-        $count++;
-      }
+for my $i (0..@grid-1) {
+  my @row = reverse @{$grid[$i]};
+  my $line;
+  for my $j (0..@row-1) {
+    my %tree = %{$row[$j]};
+    # $line .= $tree{'val'};
+    if ($tree{'val'} > $max) {
+      $max = $tree{'val'};
+      $tree{'vis'} = 1;
+      ${$grid[$i]}[@row-$j-1]{'vis'} = 1;
+      # $line .= 'V';
+      $line .= $tree{'val'};
+    } else {
+      $line .= $tree{'val'};
     }
   }
+  say $line;
+  $max = -1;
+}
 
-  my $edge_trees = scalar @grid * 4 - 4;
-  # say $count + $edge_trees;
+say '-----';
 
-}->()
+my @transposed = ();
+for my $i (0 .. $#{$grid[0]}) {
+    for my $j (0 .. $#grid) {
+        $transposed[$i][$j] = $grid[$j][$i];
+    }
+}
+
+@grid = @transposed;
+
+for my $i (0..@grid-1) {
+  my @row = @{$grid[$i]};
+  my $line;
+  for my $j (0..@row-1) {
+    my %tree = %{$row[$j]};
+    # $line .= $tree{'val'};
+    if ($tree{'val'} > $max) {
+      $max = $tree{'val'};
+      $tree{'vis'} = 1;
+      ${$grid[$i]}[$j]{'vis'} = 1;
+      # $line .= 'V';
+      $line .= $tree{'val'};
+    } else {
+      $line .= $tree{'val'};
+    }
+  }
+  say $line;
+  $max = -1;
+}
+
+
+say '-----';
+
+for my $i (0..@grid-1) {
+  my @row = reverse @{$grid[$i]};
+  my $line;
+  for my $j (0..@row-1) {
+    my %tree = %{$row[$j]};
+    # $line .= $tree{'val'};
+    if ($tree{'val'} > $max) {
+      $max = $tree{'val'};
+      $tree{'vis'} = 1;
+      ${$grid[$i]}[@row-$j-1]{'vis'} = 1;
+      # $line .= 'V';
+      $line .= $tree{'val'};
+    } else {
+      $line .= $tree{'val'};
+    }
+  }
+  say $line;
+  $max = -1;
+}
+
+
+say '-----';
+
+for my $row (@grid) {
+  my @row = @$row;
+  for my $tree (@row) {
+    my %tree = %$tree;
+    if ($tree{'vis'}) {
+      $count++;
+    }
+  }
+}
+
+say $count;
