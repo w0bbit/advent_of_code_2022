f0d909b774ff4461447997647c6ea65d7d791d81
Robert Ryan <ryanre@gmail.com>
2022-12-12 10:41:43 -0800

    update

--- day_12/day_12a.pl
+++ day_12/day_12a.pl
@@ -2,6 +2,8 @@ use v5.36;
 use warnings;
 use Mojo::Util qw(dumper);
 
+# should be 361
+
 # just setting up the grid for now
 my @grid;
 my $x = 0;
@@ -11,9 +13,9 @@ while (my $line = <$INPUT>) {
   my @vals = split '', $line;
   for my $y (0..@vals-1) {
     if ($vals[$y] eq 'S') {
-      push @grid, {'S' => [0, 0]};
+      push @grid, {'S' => [$x, $y]};
     } elsif ($vals[$y] eq 'E') {
-      push @grid, {25 => [$x, $y]};
+      push @grid, {'E' => [$x, $y]};
     } else {
       my $alt = ord($vals[$y]) - 97;
       push @grid, {$alt => [$x, $y]};
@@ -23,7 +25,15 @@ while (my $line = <$INPUT>) {
 }
 close $INPUT;
 
+my @visited;
+my @routes;
+
 # each @grid item is {altitude => [x, y]}
-for (@grid) {
-  say dumper $_;
+for my $loc (@grid) {
+  for (keys %$loc) {
+    say "$_ => [${$$loc{$_}}[0], ${$$loc{$_}}[1]]";
+  }
 }
+
+# test coords around current location to identify valid ones
+# i.e., add or subtract 1 from x coord, do same for y coord, then check that alt is not more than 1 higher than the current alt
